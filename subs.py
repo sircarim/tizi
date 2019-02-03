2ab29db80d2817a1c80482a26ac9c1190fd392c6
sircarim <kapsakarim@gmail.com>
2019-02-04 02:49:18 +0300

    Add files via upload

---
+++ subs.py
@@ -0,0 +1,9 @@
+my_string = "I love my %s" % "Wife"
+print(my_string) 
+
+var = "Kenya"
+newString = "My Country is  %s, and I love Her" % var
+print(newString) 
+
+another_string = "I love both my %s and my country %s" % ("Wife", var)
+print(another_string)  
