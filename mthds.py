305d79898275302eb72f47ddb482f003cb373227
sircarim <kapsakarim@gmail.com>
2019-02-04 04:16:32 +0300

    add sub in integer

---
+++ mthds.py
@@ -0,0 +1,7 @@
+my_string = "This is a string!"
+print(my_string.upper())
+
+# Or more concisely
+print("This is a string!".lower())
+
+#COMMENT
---
+++ subs.py
@@ -0,0 +1,24 @@
+#Substitution in strings
+my_string = "I love my %s" % "Wife"
+print(my_string) 
+
+var = "Kenya"
+newString = "My Country is  %s, and I love Her" % var
+print(newString) 
+
+another_string = "I love both my %s and my country %s" % ("Wife", var)
+print(another_string)  
+
+
+#Substitution in integer
+my_string = "%i + %i = %i" % (1,2,3)
+print(my_string) # '1 + 2 = 3'
+
+float_string = "%f" % (1.23)
+print(float_string) # '1.230000'
+
+float_string2 = "%.2f" % (1.23)
+print(float_string2) # '1.23'
+
+float_string3 = "%.2f" % (1.237)
+print(float_string3) # '1.24'
