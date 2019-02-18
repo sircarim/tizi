98cf287a0bf36f28142d82bd9aaf71a271c5aea3
sircarim <kapsakarim@gmail.com>
2019-02-18 15:43:40 +0300

    Tutorials on Dictionary
    Learning difference between list, tuple and dictionary

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+my_dict = {}
+another_dict = dict()
+my_other_dict = {"one":1, "two":2, "three":3}
+
+print(my_other_dict) # {'three': 3, 'two': 2, 'one': 1}
+dic = {"Cars":"4 seats", "mini_bus":"14 seats", "Bus":"64 seats"}
+print('\n')
+print(dic)
